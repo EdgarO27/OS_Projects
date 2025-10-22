ae59f1ad1393bbc60e208decf098d4695b9cc5ec
EdgarO27 <ortizedgar092@gmail.com>
2025-10-22 14:12:13 -0700

    Getting file reader ready for txt file

--- proj1.cpp
+++ proj1.cpp
@@ -2,22 +2,55 @@
 #include <fstream>
 #include <string>
 #include <utility>
+#include <map>
+#include <sstream>
+#include <vector>
+
+template<typename T> std::string type_name();
+
+using std::pair;
 
 int main(){
-    std::ifstream myfile;
-    std::string line;
-    int job ;
-    myfile.open("jobs.txt");
-    int time ;
-    if( myfile.is_open() ){
+    //define reading file variable
+    std::ifstream myfile("jobs.txt");
+
+    // Using map for key value pairs 
+    std::map<std::string, int> jobs;
+
+    //Still dont know how to use pairs
+    std::pair<std::string , int > pair;
+
+    // Job Name
+    std::string name;
     
-        while ( myfile.good() ){
-            
-            myfile >> line;
-            myfile >> job ;
-        }
-        std::cout << line << std::endl;
-        std::cout << job ;
-    myfile.close();
+    // index
+    int field;
+
+    // Time of Job
+    int jobTime ;
+
+    // if File doesnt exist
+    if(!myfile){
+        std::cout << " Failed to open file. \n";
     }
+
+    std::string line;
+
+    while( std::getline(myfile,line)){
+
+        std::istringstream ss(line);
+        std::string token;
+
+        std::vector<std::string> fields;
+
+        while(std::getline(ss,token,'\n')){
+            fields.push_back(token);
+        }
+
+        for (auto i : fields){
+            std::cout << i << ' ';
+        }
+    }
+    
+    
 }
