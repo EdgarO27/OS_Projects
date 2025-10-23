e3877e288304b98fff119d2a2fbfd27bbfc66165
EdgarO27 <ortizedgar092@gmail.com>
2025-10-23 11:00:55 -0700

    Pulling Home progress

--- FCFS.h
+++ FCFS.h
@@ -2,6 +2,39 @@
 #include <string>
 #include <queue>
 
+//https://stackoverflow.com/questions/10113028/how-should-i-start-making-a-fcfs-scheduling-program-in-c-or-c
+//THIS LINK IS TO HELP FOR FCFS ALGORITHM 
+
+
+/*          BATCH SYSTEM
+    To maximize jobs per hour
+    Minimize time between submisson and termination
+    Keeps CPU busy all the time
+
+    Implemented with FIFO QUEUE
+        Process enters the ready Queue
+        It is put on the end of the queue
+
+    When CPU is FREE,
+        Scheduler chooses a process form the head of a ready QUEUE
+
+    Turnaround Time is long 
+
+
+    Variables needed:
+        Process Id:
+        CPU Time
+
+    Calculations:
+        Average Turnaround Time : ( count of times per process where it ends) / # of process
+
+    Process States (BOOL):
+        Running
+        Blocked 
+        Ready
+
+*/
+
 struct Node{
 
     std::string name;
